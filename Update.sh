2e6776f2679ab226f6052b70f5b77f6bdfe2a1ce
liberodark <liberodark@gmail.com>
2016-10-11 23:36:17 +0200

    up

--- Update.sh
+++ Update.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/env bash
+# Save stdin to file descriptor 5
+exec 5<&0
+#
+# About: This is the most widely used and fastest shell script to update all your TeamSpeak 3 server instances.
+# Author: liberodark & ERGY13
+# Project: thunder-x.fr
+# License: GNU GPLv3
+
 ./ts3server_startscript.sh stop
 
 wget http://yurfile.altervista.org/download.php?fid=L1RTMy90czMudGFy
